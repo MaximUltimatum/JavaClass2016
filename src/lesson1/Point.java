45bf328892c3dc2bafbcbb983befff20e3e58186
MaximUltimatum <mcder018@live.com>
2016-02-01 13:55:50 -0500

    Set up Point Constructors and directoTo()
    Can't figure out how to compare Class accepted variables with directTo()
    accepted variables.

--- src/lesson1/Point.java
+++ src/lesson1/Point.java
@@ -1,5 +1,17 @@
 package lesson1;
 
 public class Point {
-
+	private int x;
+	private int y;
+	
+	public Point(int a, int b){
+		a = x;
+		b = y;
+	}
+	
+	public double distanceTo(Point InstantaniousClass){
+		
+		
+		return 0.0;
+	}
 }
