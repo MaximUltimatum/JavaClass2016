6b781f4b7925bcc748fa4b8be8e73d82113cc9f7
MaximUltimatum <mcder018@live.com>
2016-02-02 13:27:23 -0500

    Added Scanner function for Improved user interface
    *Trying to see if putting italics here will allow markdown to function,
    however __BOLD__ this is*

--- src/lesson1/CircleAndPointTest.java
+++ src/lesson1/CircleAndPointTest.java
@@ -1,10 +1,37 @@
 package lesson1;
+import java.util.Scanner;
 
 public class CircleAndPointTest {
+	private static Scanner pointedCircleScanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Circle c = new Circle(new Point(3, 3), 2);
-		Point p1 = new Point(1, 1);
-		Point p2 = new Point(4, 2);
+		System.out.println("Enter the integer x coordinate of the center of the circle");
+		int xCoord = pointedCircleScanner.nextInt();
+		
+		System.out.println("Enter the integer y coordinate of the center of the circle");
+		int yCoord = pointedCircleScanner.nextInt();
+		
+		System.out.println("Enter the radius of the circle");
+		double radius = pointedCircleScanner.nextDouble();
+		
+		Circle c = new Circle(new Point(xCoord, yCoord), radius);
+		
+		System.out.println("Enter the integer x coordinate of the first point");
+		int p1xCoord = pointedCircleScanner.nextInt();
+
+		
+		System.out.println("Enter the integer y coordinate of the first point");
+		int p1yCoord = pointedCircleScanner.nextInt();
+
+		System.out.println("Enter the integer x coordinate of the second point");
+		int p2xCoord = pointedCircleScanner.nextInt();
+
+		System.out.println("Enter the integer y coordinate of the second point");
+		int p2yCoord = pointedCircleScanner.nextInt();
+
+		
+		Point p1 = new Point(p1xCoord, p1yCoord);
+		Point p2 = new Point(p2xCoord, p2yCoord);
 		System.out.println("Circle c contains p1? " + c.contains(p1));
 		System.out.println("Circle c contains p2? " + c.contains(p2));
 	}
