6690bb2c535df7b5cd90fc7b24a82f842a029684
MaximUltimatum <mcder018@live.com>
2016-02-13 09:09:28 -0500

    Beginning of portfolio on this code
    I am saving this to github for use later if I don't have access to this
    computer.

--- src/myNumber/MyNumber.java
+++ src/myNumber/MyNumber.java
@@ -29,3 +29,5 @@ public class MyNumber {
 		System.out.println("The number is " + number + ".");
 	}
 }
+
+// beginning of portfolio for this code. First of all, HexadecimalNumber is a child of MyNumber because it "extends" it. This lesson was extraordinarily helpful because it showed me how the code I make in my robotics group interacts with the code we are given by FIRST, a worldwide organization that designs, programs, builds, and competes with robots.
